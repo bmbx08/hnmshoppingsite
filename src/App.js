0ef35240391c536cb8ee80ac8d75d15c2661ff83
bmbx08 <bmbx08@bu.ac.kr>
2024-07-16 23:16:29 +0900

    added navbar,db.json

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import { Route,Routes } from 'react-router';
 import ProductAll from './page/ProductAll';
 import Login from './page/Login';
 import ProductDetail from './page/ProductDetail';
+import Navbar from './component/Navbar';
 
 //1. 전체상품페이지, 로그인, 상품상세페이지
+//1-1. 네비게이션 바
 //2. 전체 상품페이지에서는 전체 상품을 볼 수 있다.
 //3. 로그인 버튼을 누르면 로그인 페이지가 나온다.
 //4. 상품디테일을 눌렀으나, 로그인이 안되있을 경우에는 로그인페이지가 먼저 나온다.
@@ -13,9 +15,12 @@ import ProductDetail from './page/ProductDetail';
 //7. 상품 디테일 페이지에서 로그아웃하면 상품디테일 페이지 볼 수 없음. 다시 로그인 페이지로 리다이렉트.
 //8. 로그인을 하면 로그아웃이 보이고 로그아웃을 하면 로그인이 보인다.
 //9. 상품을 검색 할 수 있다.
+
+//json server실행 명령어: npx json-server --watch db.json --port 5000
 function App() {
   return (
     <div>
+      <Navbar/>
       <Routes>
         <Route path="/" element={<ProductAll/>}/>
         <Route path="/login" element={<Login/>}/>
---
+++ src/component/ProductCard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ProductCard = () => {
+  return (
+    <div>
+      <img width="300px" src="https://lp2.hm.com/hmgoepprod?set=source[/64/c3/64c32e94928bb92df3eacb735128c956f7f4b62f.jpg],origin[dam],category[],type[LOOKBOOK],res[w],hmver[1]&call=url[file:/product/main]"/>
+      <div>Conscious choice</div>
+      <div>Title</div>
+      <div>₩99900</div>
+      <div>신제품</div>
+    </div>
+  )
+}
+
+export default ProductCard
